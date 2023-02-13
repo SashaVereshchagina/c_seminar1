a05fc94b8720de2d1302565aecc497c92a976274
Aleksandra Vereshchagina <alexvereschagina@gmail.com>
2023-02-13 22:38:50 +0100

    Task 10

---
+++ task10/Program.cs
@@ -0,0 +1,16 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.WriteLine("Что будем делить? ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("На что будем делить? ");
+int b = int.Parse(Console.ReadLine());
+
+if (a % b == 0)
+{
+    Console.WriteLine(a + " кратно " + b);
+}
+else  {
+    int res = a % b;
+    Console.WriteLine(a + " не кратно " + b + ", остаток: " + res);
+}
