c3fe8899c0acfb2b43f36a7c849349f0c16c039e
Aleksandra Vereshchagina <alexvereschagina@gmail.com>
2023-02-13 21:58:08 +0100

    Task 5

---
+++ task5/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать четные числа от 1 до N
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+int i = 1;
+
+while (i <= n)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+    i = i+1;
+}
