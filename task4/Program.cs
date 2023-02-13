9ec2b4306b7eefa873960b583791cb2c8ddf4576
Aleksandra Vereshchagina <alexvereschagina@gmail.com>
2023-02-13 21:49:56 +0100

    Task 4

---
+++ task4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Выяснить является ли число чётным
+
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine(a + " - четное число");
+}
+else  Console.WriteLine(a + " - нечетное число");
