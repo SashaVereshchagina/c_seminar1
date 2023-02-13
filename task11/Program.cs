0c86d35de1958e278de55262e46f8c260d8a79c8
Aleksandra Vereshchagina <alexvereschagina@gmail.com>
2023-02-13 23:04:36 +0100

    Task 11

---
+++ task11/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+Console.WriteLine("Введите число: ");
+
+string n = Console.ReadLine();
+
+int i = n.Length;
+
+if (i<3) Console.WriteLine("Третьей цифры нет");
+else {
+        Console.WriteLine(n[2]); //Console.WriteLine(n.Substring(2, 1));
+}
