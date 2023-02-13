a73ffdc04d77431c8f68b0a62fb315518881dc38
Aleksandra Vereshchagina <alexvereschagina@gmail.com>
2023-02-13 22:10:19 +0100

    Task 6

---
+++ task6/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+Console.WriteLine("Введите трехзначное число: ");
+int n = int.Parse(Console.ReadLine());
+
+int i = (n%10);
+
+Console.WriteLine("Последняя цифра: " + i);
