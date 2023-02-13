721c59a5078716d89d4d1f22271418235de4c30a
Aleksandra Vereshchagina <alexvereschagina@gmail.com>
2023-02-13 22:16:15 +0100

    Task 7

---
+++ task7/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.WriteLine("Введите трехзначное число: ");
+int n = int.Parse(Console.ReadLine());
+
+int i = ((n%100)-(n%10))/10;
+
+Console.WriteLine("Вторая цифра: " + i);
