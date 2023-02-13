e764638d2507e0889779435ab977c9e03943a8ea
Aleksandra Vereshchagina <alexvereschagina@gmail.com>
2023-02-13 22:32:32 +0100

    Task 9

---
+++ task9/Program.cs
@@ -0,0 +1,8 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+Console.WriteLine("Введите трехзначное число: ");
+int n = int.Parse(Console.ReadLine());
+
+n = (n/100)*10+(n%10);
+
+Console.WriteLine("Новое число: " + n);
